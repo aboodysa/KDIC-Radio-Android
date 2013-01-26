c1d6878b2d5f7e7e9c6763df04bf828fbdc48f9f
Travis Law <Travis.e.law@gmail.com>
2013-01-26 15:07:22 -0600

    Started implementing a ViewPager system for the browser. NOT working yet, but will hopefully finish tomorrow with Spencer. We are going to have the browser navigate via swipes and button clicks in the wakizashi & banner activities.

---
+++ src/edu/grinnell/kdic/browser/BrowserPager.java
@@ -0,0 +1,11 @@
+package edu.grinnell.kdic.browser;
+
+import android.app.Activity;
+import android.support.v4.view.ViewPager;
+
+public class BrowserPager extends Activity {
+	ViewPager mViewPager;
+	
+	
+
+}
