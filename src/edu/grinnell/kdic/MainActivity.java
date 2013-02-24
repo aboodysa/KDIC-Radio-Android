13b768f8b9b6c8a1a223b2f8246713e7ec73ad10
Spencer B Liberto <libertos@grinnell.edu>
2013-02-24 14:01:10 -0600

    Polished the browser. It should now be testable.

--- src/edu/grinnell/kdic/MainActivity.java
+++ src/edu/grinnell/kdic/MainActivity.java
@@ -1,32 +1,43 @@
 package edu.grinnell.kdic;
 
+import java.util.ArrayList;
 
-
-import edu.grinnell.kdic.browser.BrowserViewPager;
+import edu.grinnell.kdic.browser.BrowserPagerAdapter;
 import edu.grinnell.kdic.streambanner.StreamBannerFragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 //import android.net.Uri;
 //import android.widget.ImageView;
 //import android.widget.TextView;
 import android.support.v4.app.FragmentActivity;
-
-
+import android.support.v4.view.ViewPager;
 
 public class MainActivity extends FragmentActivity {
+
+	static ArrayList<Fragment> mFrags;
+    private ViewPager 			mBrowserPager;
+    private BrowserPagerAdapter	mBrowserPagerAdapter;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        createBlankFragArray();
         	
         getSupportFragmentManager().beginTransaction()
         	.replace(R.id.radio_banner_container, new StreamBannerFragment())
         	.commit();
         
-        getSupportFragmentManager().beginTransaction()
-    	.replace(R.id.radio_banner_container, new BrowserViewPager(null))
-    	.commit();
+        mBrowserPager = (ViewPager) findViewById(R.id.browser_pager);
+        mBrowserPagerAdapter = new BrowserPagerAdapter(null, mFrags);
+        mBrowserPager.setAdapter(mBrowserPagerAdapter);
         
+    }
+    
+	public static void createBlankFragArray(){
+    	for(int i=0; i<3; i++)
+    		mFrags.add(new Fragment());
     }
     
 }
